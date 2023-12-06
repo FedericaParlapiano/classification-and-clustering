4408331adee7e83d58ae245ff8fc5b76d7b29a8f
AriannaRonci <s1093926@studenti.univpm.it>
2023-12-06 18:47:04 +0100

    file preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+
+def processing():
+    df = pd.read_csv('data/ObesityDataSet.csv')
+
+    df['FAVC'] = df['FAVC'].replace({'yes': 1, 'no': 0})
+    df['SMOKE'] = df['SMOKE'].replace({'yes': 1, 'no': 0})
+    df['SCC'] = df['SCC'].replace({'yes': 1, 'no': 0})
+
+    df['CALC'] = df['CALC'].replace({'no': 0, 'Sometimes': 1, 'Frequently': 2, 'Always': 3})
+    df['CAEC'] = df['CAEC'].replace({'no': 0, 'Sometimes': 1, 'Frequently': 2, 'Always': 3})
+
+    df = df.rename(columns={"family_history_with_overweight": "Family History Of Overweight",
+                            "FAVC": "High Caloric Food Consumption",
+                            "FCVC": "Vegetables Consumption",
+                            "NCP": "Main Meals Number",
+                            "CAEC": "Food Consumption Between Meals",
+                            "SMOKE": "Smoke",
+                            "CH20": "Daily Water Consumption",
+                            "SSC": "Calories Consumption Monitoring",
+                            "FAF": "Physical Activity Frequency",
+                            "TUE": "Time Using Technology Devices",
+                            "CALC": "Alcohol Consumption",
+                            "MTRANS": "Transportation Used",
+                            "NObesidad": "Nutritional Status"})
+
+    df.to_csv('data\obesity_dataset_clean.csv', header=True)
+
+
+if __name__ == '__main__':
+    processing()
