c35815a61d2e1dd5ba2d29c4b963734fb91ec333
chiara <s1093786@studenti.univpm.it>
2023-12-06 19:56:31 +0100

    commit

--- analisi_descrittiva.py
+++ analisi_descrittiva.py
@@ -1,8 +1,12 @@
+from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
 
+file = 'data/obesity_dataset_clean.csv'
+obesity = pd.read_csv(file)
+obesity_no_index = obesity.iloc[:, 1:]
 
 def hist():
     df = pd.read_csv('data/obesity_dataset_clean.csv')
@@ -20,3 +24,21 @@ def hist():
 
     # Mostra il grafico
     plt.show()
+
+def plot_correlation_matrix(df):
+    enc = LabelEncoder()
+    columns_to_encode = ["Gender", "Family History Of Overweight", "Transportation Used", "Nutritional Status"]
+
+    for col in columns_to_encode:
+        df[col] = enc.fit_transform(df[col])
+
+    corr = df.corr()
+    plt.figure(figsize=(20, 16))
+    sns.heatmap(corr, annot=True, cmap='crest')
+
+    plt.title('Correlation Matrix')
+    plt.savefig('Correlation Matrix', bbox_inches='tight')
+    plt.show()
+
+
+plot_correlation_matrix(obesity_no_index)
