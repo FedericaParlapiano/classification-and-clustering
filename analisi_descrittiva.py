7fd993da33759ebd636453a2880d994d4c05b4d1
AriannaRonci <s1093926@studenti.univpm.it>
2023-12-06 22:26:49 +0100

    funzione istogramma per la distribuzione del peso, kde per BMI e pie chart per stato nutrizionale

--- analisi_descrittiva.py
+++ analisi_descrittiva.py
@@ -9,21 +9,53 @@ obesity = pd.read_csv(file)
 obesity_no_index = obesity.iloc[:, 1:]
 
 
-def hist():
-    df = pd.read_csv('data/obesity_dataset_clean.csv')
+def weight_height():
+    plt.figure(figsize=(12, 6))  # Imposta le dimensioni della figura
 
     # calcolo larghezza del bin secondo la regola di Scott
-    larghezza_bin_scott = 3.5 * np.std(df['Weight']) / (len(df['Weight']) ** (1 / 3))
+    larghezza_bin_weight = 3.5 * np.std(obesity['Weight']) / (len(obesity['Weight']) ** (1 / 3))
+    larghezza_bin_height = 3.5 * np.std(obesity['Height']) / (len(obesity['Height']) ** (1 / 3))
     # calcolo numero di bin
-    numero_bin_scott = int((max(df['Weight']) - min(df['Weight'])) / larghezza_bin_scott)
+    numero_bin_weight = int((max(obesity['Weight']) - min(obesity['Weight'])) / larghezza_bin_weight)
+    numero_bin_height = int((max(obesity['Height']) - min(obesity['Height'])) / larghezza_bin_height)
 
-    plt.hist(df['Weight'], bins=numero_bin_scott, color='blue', edgecolor='black')
-
+    plt.subplot(1, 2, 1)
+    sns.histplot(obesity['Weight'], bins=numero_bin_weight, color='#2D9596', kde=True, alpha=0.3)
     plt.xlabel('Peso')
     plt.ylabel('Frequenza')
     plt.title('Distribuzione del peso')
 
-    # Mostra il grafico
+    plt.subplot(1, 2, 2)
+    sns.histplot(obesity['Height'], bins=numero_bin_height, color='#82A0D8', kde=True)
+    plt.xlabel('Altezza')
+    plt.ylabel('Frequenza')
+    plt.title('Distribuzione dell\'altezza')
+
+    plt.show()
+
+
+def BMI():
+    obesity['BMI'] = obesity['Weight'] / obesity['Height'] ** 2
+
+    sns.color_palette('Paired')
+    sns.kdeplot(obesity, x=obesity['BMI'], hue=obesity['Gender'], fill=True)
+    plt.ylabel('Densità')
+    plt.title('Distribuzione dell\'indice di massa corporea')
+
+    plt.show()
+
+
+def pie_chart():
+    stato_nutrizionale = obesity['Nutritional Status'].value_counts()
+    labels = ['Insufficient Weight', 'Normal Weight', 'Overweight Level I', 'Overweight Level II', 'Obesity Type I',
+              'Obesity Type II', 'Obesity Type III']
+    count = [stato_nutrizionale.get('Insufficient_Weight', 0), stato_nutrizionale.get('Normal_Weight', 0),
+             stato_nutrizionale.get('Overweight_Level_I', 0), stato_nutrizionale.get('Overweight_Level_II', 0),
+             stato_nutrizionale.get('Obesity_Type_I', 0), stato_nutrizionale.get('Obesity_Type_II', 0),
+             stato_nutrizionale.get('Obesity_Type_III', 0)]
+    colors = sns.color_palette('Paired')[0:10]
+    plt.pie(count, colors=colors, autopct='%.0f%%', labels=None)
+    plt.legend(labels, loc='best')
     plt.show()
 
 
@@ -35,7 +67,8 @@ def plot_correlation_matrix(df):
         df[col] = enc.fit_transform(df[col])
 
     corr = df.corr()
-    plt.figure(figsize=(20, 16))
+    plt.figure(figsize=(16, 12))
+    sns.color_palette('Paired')
     sns.heatmap(corr, annot=True, cmap='crest')
 
     plt.title('Correlation Matrix')
@@ -43,4 +76,7 @@ def plot_correlation_matrix(df):
     plt.show()
 
 
+weight_height()
+BMI()
+pie_chart()
 plot_correlation_matrix(obesity_no_index)
